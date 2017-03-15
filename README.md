248878928212c405315190d045881caccafd7367
Sami Patanen <sami.patanen@navtureapps.com>
2017-03-15 15:03:01 +0200

    Updated to 2.5.

--- README.md
+++ README.md
@@ -2,25 +2,24 @@
 
 Welcome!
 
-This is a simple working demonstration of Proximi.io platform in action on Android.
+This is a simple working demonstration of Proximi.io platform in action on Android,
+that demonstrates basic usage of Proximi.io, usage of Google Maps with Proximi.io, and generic push notifications through Proximi.io.
 Our complete Android documentation can be found [here](https://proximi.io/docs/android/).
 
 ### How to use
 
-Note: The following two steps are required and the errors you get during the first build are intentional! If you don't define the email/password & Google API key, the build will result in an error. In case you don't want to obtain the Google API key, you can use empty value to get the project to build. In this case the map tiles will not show up but the project will build without error. 
+Note: The following two steps are required and the errors you get during the first build are intentional! If you don't define the email/password & Google API key, the build will result in an error. In case you don't want to obtain the Google API key, you can use empty value to get the project to build. In this case the map tiles will not show up but the project will build without error.
 
-Clone or download this repo and open it with Android Studio as an existing project. After that you need to replace the placeholders in the following section with your credentials (MainActivity.java):
-
-```
-// Login to Proximi.io
-proximiio.setLogin(EMAIL, PASSWORD);
-```
-
-As an alternative way for logging in, you can also use an auth token instead of the email & password:
+Clone or download this repo and open it with Android Studio as an existing project. After that you need to replace the placeholders in the following section with your credentials (MainActivityListener.java):
 
 ```
-// Login to Proximi.io
-proximiio.setAuth("exampleauthkey");
+proximiioAPI.setAuth(AUTH_KEY);
+```
+
+As an alternative way for logging in, you can also use email & password instead of an auth token:
+
+```
+proximiioAPI.setLogin(EMAIL, PASSWORD);
 ```
 
 You also need to edit the AndroidManifest.xml and replace the Google API Key:
--- app/src/main/java/io/proximi/proximiiodemo/MainActivity.java
+++ app/src/main/java/io/proximi/proximiiodemo/MainActivity.java
@@ -6,26 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-import io.proximi.proximiiolibrary.Proximiio;
-import io.proximi.proximiiolibrary.ProximiioFactory;
 import io.proximi.proximiiolibrary.ProximiioFloor;
-import io.proximi.proximiiolibrary.ProximiioGeofence;
 import io.proximi.proximiiolibrary.ProximiioGoogleMapHelper;
-import io.proximi.proximiiolibrary.ProximiioListener;
 
 /**
  * Proximiio Demo
  */
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private Proximiio proximiio;
-    private ProximiioListener listener;
+    private MainActivityListener listener;
     @Nullable private ProximiioGoogleMapHelper mapHelper;
     private Toolbar toolbar;
 
@@ -36,34 +30,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a Proximiio instance
-        proximiio = ProximiioFactory.getProximiio(this);
-
-        // Create a ProximiioListener and add it to Proximiio
-        listener = new ProximiioListener() {
-            @Override
-            public void geofenceEnter(ProximiioGeofence geofence) {
-                Log.d(TAG, "Geofence enter: " + geofence.getName());
-            }
-
-            @Override
-            public void geofenceExit(ProximiioGeofence geofence, @Nullable Long dwellTime) {
-                Log.d(TAG, "Geofence exit: " + geofence.getName() + ", dwell time: " + String.valueOf(dwellTime));
-            }
-
-            @Override
-            public void loginFailed(LoginError loginError) {
-                Log.e(TAG, "LoginError! (" + loginError.toString() + ")");
-            }
-        };
-
-        proximiio.setActivity(this);
-        proximiio.addListener(listener);
-
-        // Login to Proximi.io
-        proximiio.setLogin(EMAIL, PASSWORD);
-
-        proximiio.checkPermissions();
+        // Create our Proximi.io listener
+        listener = new MainActivityListener(this);
 
         // Set toolbar buttons to change the current floor up and down
         findViewById(R.id.floorUp).setOnClickListener(new View.OnClickListener() {
@@ -98,20 +66,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (mapHelper != null) {
             mapHelper.destroy();
         }
-        proximiio.removeListener(listener);
-        proximiio.removeActivity(this);
+        listener.onDestroy();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        proximiio.onActivityResult(requestCode, resultCode, data);
+        listener.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        proximiio.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     // Called when the map is ready to use
