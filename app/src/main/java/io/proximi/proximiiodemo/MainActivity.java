296a4ea9ab535063e3487efba22d1f03990ca7bd
Sami Patanen <sami.patanen@navtureapps.com>
2017-05-23 13:48:12 +0300

    2.6

--- app/src/main/java/io/proximi/proximiiodemo/MainActivity.java
+++ app/src/main/java/io/proximi/proximiiodemo/MainActivity.java
@@ -6,24 +6,29 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import io.proximi.proximiiolibrary.ProximiioAPI;
 import io.proximi.proximiiolibrary.ProximiioFloor;
+import io.proximi.proximiiolibrary.ProximiioGeofence;
 import io.proximi.proximiiolibrary.ProximiioGoogleMapHelper;
+import io.proximi.proximiiolibrary.ProximiioListener;
 
 /**
  * Proximiio Demo
  */
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private MainActivityListener listener;
+    private ProximiioAPI proximiioAPI;
     @Nullable private ProximiioGoogleMapHelper mapHelper;
     private Toolbar toolbar;
 
-    private final static String TAG = "ProximiioDemo";
+    private static final String TAG = "ProximiioDemo";
+    private static final String AUTH = "AUTH_KEY_HERE"; // TODO: Replace with your own!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         // Create our Proximi.io listener
-        listener = new MainActivityListener(this);
+        proximiioAPI = new ProximiioAPI(TAG, this);
+        proximiioAPI.setListener(new ProximiioListener() {
+            @Override
+            public void geofenceEnter(ProximiioGeofence geofence) {
+                Log.d(TAG, "Geofence enter: " + geofence.getName());
+            }
+
+            @Override
+            public void geofenceExit(ProximiioGeofence geofence, @Nullable Long dwellTime) {
+                Log.d(TAG, "Geofence exit: " + geofence.getName() + ", dwell time: " + String.valueOf(dwellTime));
+            }
+
+            @Override
+            public void loginFailed(LoginError loginError) {
+                Log.e(TAG, "LoginError! (" + loginError.toString() + ")");
+            }
+        });
+        proximiioAPI.setAuth(AUTH);
+        proximiioAPI.setActivity(this);
 
         // Set toolbar buttons to change the current floor up and down
         findViewById(R.id.floorUp).setOnClickListener(new View.OnClickListener() {
@@ -66,19 +89,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (mapHelper != null) {
             mapHelper.destroy();
         }
-        listener.onDestroy();
+        proximiioAPI.destroy();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        listener.onActivityResult(requestCode, resultCode, data);
+        proximiioAPI.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        proximiioAPI.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     // Called when the map is ready to use
